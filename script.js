b12ca7df47261dcfbfd8c07f74cdb0ade34bb327
yourUsername <your_email@example.com>
2016-12-02 10:58:11 -0500

    fixed question 3

--- script.js
+++ script.js
@@ -9,20 +9,49 @@ let score = 0; //this is your variable
 $futureButton = $('.future');
 $interButton = $('.interstellar');
 $thgButton = $('.thg');
-$liButton = $('li');
 
-$('.correct').click(function(){
+$('.q1 .correct').click(function(){
 score++;
 $(".myScore").html(score);
 alert("Correct"); //anytime you want to access a variable - no quotes
+$(".q1").remove();
+
 
 });
 
-$('.wrong').click(function(){
+$('.q1 .wrong').click(function(){
 alert('wrong');
+$(".q1").hide();
+});
+
+
+$('.q2 .correct').click(function(){
+score++;
+$(".myScore").html(score);
+alert("Correct"); //anytime you want to access a variable - no quotes
+$(".q2").remove();
+
 
 });
 
+$('.q2 .wrong').click(function(){
+alert('wrong');
+$(".q2").hide();
+});
+
+$('.q3 .correct').click(function(){
+score++;
+$(".myScore").html(score);
+alert("Correct"); //anytime you want to access a variable - no quotes
+$(".q3").remove();
+
+
+});
+
+$('.q3 .wrong').click(function(){
+alert('wrong');
+$(".q3").hide();
+});
 
 
 
@@ -35,15 +64,15 @@ $thgButton.on("click","");
 // When future question() is clicked {we want this to happen}
 // prompt is being stored in userAnswer
 
- function futureQuestions(){
-  var userAnswer = prompt("What is the name of the mall?");
-  if (userAnswer == "Pine Mall"){
-    console.log(userAnswer);
-    alert("You got it!");
-}
-  else {
-    console.log("nah chill");
-    alert("Not Quite");
-  }
-
-}
+ function futureQuestions(){};
+//   var userAnswer = prompt("What is the name of the mall?");
+//   if (userAnswer == "Pine Mall"){
+//     console.log(userAnswer);
+//     alert("You got it!");
+// }
+//   else {
+//     console.log("nah chill");
+//     alert("Not Quite");
+//   }
+//
+// }
